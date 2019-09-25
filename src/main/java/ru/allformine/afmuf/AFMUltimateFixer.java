7c93e77eeaf5e93d94d67adac2367fe60f7f098e
Rob <rob.bog@list.ru>
2019-09-25 14:59:21 +0300

    serverSideOnly мод

--- src/main/java/ru/allformine/afmuf/AFMUltimateFixer.java
+++ src/main/java/ru/allformine/afmuf/AFMUltimateFixer.java
@@ -5,7 +5,7 @@ import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = "afmuf", certificateFingerprint = "9f0ef428570ea3dc9744a329e6e589d21413fe18")
+@Mod(modid = "afmuf", certificateFingerprint = "9f0ef428570ea3dc9744a329e6e589d21413fe18", serverSideOnly = true, acceptableRemoteVersions = "*")
 public class AFMUltimateFixer {
     public static Logger logger;
 
