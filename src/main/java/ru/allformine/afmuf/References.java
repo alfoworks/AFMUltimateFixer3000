7e1b36ade95c2ed284231055c9e9c1b22d8f59f7
Rob <rob.bog@list.ru>
2019-09-27 19:40:14 +0300

    очистка лишнего кода хера

--- src/main/java/ru/allformine/afmuf/References.java
+++ src/main/java/ru/allformine/afmuf/References.java
@@ -1,14 +1,6 @@
 package ru.allformine.afmuf;
 
-import net.minecraftforge.common.config.Property;
-
 @SuppressWarnings("ALL")
 public class References {
-    public static Property server_id;
-    public static final String server_id_default = "PLACE YOUR SERVER_ID";
-    public static Property webhook_api_domain;
-    public static final String webhook_api_domain_default = "allformine.ru";
-
-    // Service variable
-    public static Property checked_webhook_api_default_value;
+    public static String server_id = "";
 }
--- src/main/java/ru/allformine/afmuf/net/discord/Webhook.java
+++ src/main/java/ru/allformine/afmuf/net/discord/Webhook.java
@@ -8,12 +8,12 @@ import ru.allformine.afmuf.net.Requests;
 
 public class Webhook {
     private static void sendApiRequest(JsonObject object, String[] extra) {
-        object.addProperty("server_id", References.server_id.getString());
+        object.addProperty("server_id", References.server_id);
         object.addProperty("type", "PACKETHACK_USAGE_DETECTED");
         object.addProperty("group", "secalert");
         object.add("arguments", arrayToJson(extra));
         final String json = object.toString();
-        Requests.sendPostJSON(json, "https://" + References.webhook_api_domain.getString() + "/webhook_api/");
+        Requests.sendPostJSON(json, "https://allformine.ru/webhook_api/");
     }
 
     private static JsonArray arrayToJson(String[] array) {
@@ -24,7 +24,7 @@ public class Webhook {
         return jsonArray;
     }
 
-    public static void sendSecureAlert(AlertContext context) { //TODO дописать это. Сделаю это сам (не выполнять)
+    public static void sendSecureAlert(AlertContext context) {
         JsonObject object = new JsonObject();
 
         sendApiRequest(object, new String[]{context.playerName, context.mod.getModName(), context.packetName, context.getExtraInfo()});
