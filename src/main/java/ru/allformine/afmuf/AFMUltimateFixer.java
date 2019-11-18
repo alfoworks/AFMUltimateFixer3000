cbf799a8fe580b375f2de27055229ce83e7350c0
Rob <rob.bog@list.ru>
2019-11-18 23:31:33 +0300

    удалил запущенный дебаг принт

--- src/main/java/ru/allformine/afmuf/AFMUltimateFixer.java
+++ src/main/java/ru/allformine/afmuf/AFMUltimateFixer.java
@@ -4,12 +4,9 @@ import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLServerAboutToStartEvent;
 import org.apache.logging.log4j.Logger;
-import pcl.opensecurity.common.tileentity.TileEntityEntityDetector;
 
 import java.io.File;
-import java.lang.reflect.Method;
 
 @Mod(modid = "afmuf", certificateFingerprint = "9f0ef428570ea3dc9744a329e6e589d21413fe18", serverSideOnly = true, acceptableRemoteVersions = "*")
 public class AFMUltimateFixer {
@@ -26,12 +23,5 @@ public class AFMUltimateFixer {
         config.load();
 
         References.server_id = config.getString("Discord", "server_id", References.server_id, "Server ID");
-    }
-
-    @EventHandler
-    public void serverStart(FMLServerAboutToStartEvent event) {
-        for (Method method : TileEntityEntityDetector.class.getMethods()) {
-            System.out.println(method.toString());
-        }
     }
 }
